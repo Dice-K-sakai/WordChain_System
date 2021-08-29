7844d955832c9466ffdb0ddfe4761350cd40512a
daisu <daisu@DIE-SKL>
2021-08-29 23:46:06 +0900

    プロジェクト ファイルを追加します。

---
+++ WordChain_System/ChatSystem.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WordChain_System
+{
+    class ChatSystem
+    {
+        private string _hostName;
+
+        public string hostName
+        {
+            get => _hostName;
+        }
+
+        public enum ConnectMode { host = 0, client = 1 };
+        private ConnectMode _connectMode;
+
+        public ConnectMode connectMode
+        {
+            get => _connectMode;
+        }
+
+        private IPAddress _ipAddress;
+        private Int32 _portNo;
+        private IPEndPoint _localEndPoint;
+        private Socket _connectSocet = null;
+        private Socket _chatSocket = null;
+        private int _maxChatLength;
+
+        // termination of processes
+        public enum EResult { success, exception, socketException, argumentOutOfRangeException, notInit };
+        EResult eResult = EResult.success;
+        public Exception exception;
+        public SocketException socketException;
+        public ArgumentOutOfRangeException argumentOutOfRangeException;
+
+        public string resultMessage
+        {
+            get
+            {
+                string s = null;
+
+                switch (eResult)
+                {
+                    case EResult.success:
+                        s = "成功";
+                        break;
+                    case EResult.exception:
+                        s = "例外:" + exception.Message;
+                        break;
+                    case EResult.socketException:
+                        s = "ソケットでの例外" + socketException.Message;
+                        break;
+                    case EResult.argumentOutOfRangeException:
+                        s = "引数が範囲外の例外:" + argumentOutOfRangeException.Message;
+                        break;
+                    case EResult.notInit:
+                        s = "初期化されていません。";
+                        break;
+                    default:
+                        break;
+                }
+
+                return s;
+            }
+        }
+        public class Buffer
+        {
+            public int length;
+            public int capacity;
+            public byte[] content;
+            public Buffer(int c)
+            {
+                capacity = c;
+                content = new byte[capacity];
+                length = 0;
+            }
+        }
+
+
+        public ChatSystem(int maxChatLength = 0)
+        {
+            _maxChatLength = maxChatLength;
+            _hostName = Dns.GetHostName();
+        }
+
+        /// <summary>
+        /// Initialize as a Host
+        /// </summary>
+        /// <param name="ipAddress">ip Addressa</param>
+        /// <param name="portNo"> port No</param>
+
+        public EResult InitializeHost(IPAddress ipAddress, Int32 portNo)
+        {
+            _connectMode = ConnectMode.host;
+            _ipAddress = ipAddress;
+            _portNo = portNo;
+            _localEndPoint = new IPEndPoint(ipAddress, portNo);
+
+            //接続のためのソケットを作成
+            _connectSocet = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //通信の受け入れ準備
+            try
+            {
+                _connectSocet.Bind(_localEndPoint);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+                return EResult.exception;
+            }
+
+            try
+            {
+                _connectSocet.Listen(10);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+                return EResult.exception;
+            }
+
+            //通信の確立
+            try
+            {
+                _chatSocket = _connectSocet.Accept();
+            }
+            catch (Exception e)
+            {
+                exception = e;
+                return EResult.exception;
+            }
+
+            return EResult.success;
+        }
+        /// <summary>
+        /// Initialize as a Client
+        /// </summary>
+        /// <param name="ipAddress">ipAddress</param>
+        /// <param name="portNo">portNo</param>
+        /// <param name="e">Exception</param>
+        /// <returns>bool result</returns>
+
+        public EResult InitializeClient(IPAddress ipAddress, Int32 portNo)
+        {
+            _connectMode = ConnectMode.client;
+            _ipAddress = ipAddress;
+            _portNo = portNo;
+            _localEndPoint = new IPEndPoint(ipAddress, portNo);
+            //ソケットを作成
+            _connectSocet = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //接続する。失敗するとエラーで落ちる。
+            try
+            {
+                _connectSocet.Connect(_localEndPoint);
+            }
+            catch (Exception e)
+            {
+
+                exception = e;
+                return EResult.exception;
+            }
+
+            _chatSocket = _connectSocet;
+
+            return EResult.success;
+        }
+
+        /// <summary>
+        /// Receive connected Socket
+        /// </summary>
+        /// <returns>Suceed ,received string or ErrorMessage</returns>
+
+        public EResult Receive(Buffer buffer)
+        {
+            if (_chatSocket != null)
+            {
+                // 初期化済み
+                int bytesRec;
+
+                try
+                {
+                    bytesRec = _chatSocket.Receive(buffer.content, buffer.capacity, SocketFlags.None);
+                }
+                catch (SocketException e)
+                {
+                    _chatSocket = null;
+                    _connectSocet = null;
+                    socketException = e;
+                    return EResult.socketException;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    argumentOutOfRangeException = e;
+
+                    return EResult.argumentOutOfRangeException;
+                }
+
+                // 正常に受信
+                return EResult.success;
+            }
+            else
+            {
+                return EResult.notInit;
+            }
+        }
+        public EResult Send(Buffer buffer)
+        {
+            try
+            {
+                _chatSocket.Send(buffer.content, buffer.length, SocketFlags.None);
+            }
+            catch (SocketException e)
+            {
+                //ソケットへのアクセスを試行しているときにエラーが発生しました。
+                _chatSocket = null;
+                _connectSocet = null;
+                socketException = e;
+
+                return EResult.socketException;
+            }
+
+            return EResult.success;
+        }
+
+        public void ShutDownColse()
+        {
+            if (_connectSocet != null)
+            {
+                try
+                {
+                    _connectSocet.Shutdown(SocketShutdown.Both);
+                }
+                catch (SocketException)
+                {
+
+                }
+
+                _connectSocet.Close();
+                _connectSocet = null;
+            }
+
+            return;
+        }
+    }
+
+}
---
+++ WordChain_System/Main.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using WordChain_System;
+
+namespace WordChain_System
+{
+    class main
+    {
+        static ChatSystem chatSystem;
+        const Int32 portNo = 11000;
+        const string EOF = "<EOF>";
+        static readonly int maxLength = 200 + EOF.Length;
+        static ChatSystem.ConnectMode connectMode;
+
+        static string user_name = "";
+
+        static void Main(string[] args)
+        {
+            chatSystem = new ChatSystem(maxLength);
+            Console.WriteLine($"このPCのホスト名は {chatSystem.hostName}です。");
+
+            while (true)
+            {
+                Console.Write("名前を入力してください。:");
+                user_name = Console.ReadLine();
+
+                if (user_name != "")
+                {
+                    user_name += ":";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("名前が検出できませんでした。もう一度入力してください。\n");
+                }
+
+            }
+
+            connectMode = SelectMode();
+            InChat();
+
+        }
+
+        static ChatSystem.ConnectMode SelectMode()
+        {
+            ChatSystem.ConnectMode connectMode = ChatSystem.ConnectMode.host;
+
+            while (true)
+            {
+                Console.Write("モードを選択してください。[ 0か1を入力 ]\n{ 0 : Host , 1 : Client } : ");
+                int select = int.Parse(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 0:
+
+                        //Host
+                        Console.WriteLine("ホストモードで起動します。");
+                        InitializeHost();
+                        connectMode = ChatSystem.ConnectMode.host;
+                        break;
+
+                    case 1:
+
+                        //Client
+                        Console.WriteLine("クライアントモードで起動します。");
+                        InitializeClient();
+                        connectMode = ChatSystem.ConnectMode.client;
+                        break;
+
+                    default:
+
+                        Console.WriteLine("入力が未定義でした。もう一度入力してください。\n");
+                        break;
+                }
+
+                if (select == 0 || select == 1)
+                {
+                    break;
+                }
+            }
+
+            return connectMode;
+        }
+
+        static void InitializeHost()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(chatSystem.hostName);
+
+            foreach (var addresslist in ipHostInfo.AddressList)
+            {
+                Console.WriteLine($"自分のアドレスが見つかりました:{addresslist.ToString()}");
+            }
+
+            int address_Select = 0;
+            while (true)
+            {
+                Console.Write($"\n公開するアドレスを選択してください。(0 から {ipHostInfo.AddressList.Length - 1}):");
+
+                address_Select = int.Parse(Console.ReadLine());
+
+                if (address_Select >= 0 && address_Select <= ipHostInfo.AddressList.Length - 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("例外が入力されました。もう一度入力してください。");
+                }
+            }
+
+            Console.WriteLine("クライアント接続待ち…");
+
+            IPAddress ipAddress = ipHostInfo.AddressList[address_Select];
+            ChatSystem.EResult re = chatSystem.InitializeHost(ipAddress, portNo);
+
+            Console.WriteLine("\n\n\n");//改行
+
+            if (re != ChatSystem.EResult.success)
+            {
+                Console.WriteLine($"初期化に失敗しました。\nエラー内容 = {re.ToString()}");
+            }
+
+        }
+
+        static void InitializeClient()
+        {
+            Console.Write("接続するIPアドレスを入力してください。:");
+            var ipAddress = IPAddress.Parse(Console.ReadLine());
+            ChatSystem.EResult re = chatSystem.InitializeClient(ipAddress, 11000);
+
+            if (re == ChatSystem.EResult.success)
+            {
+                Console.WriteLine($"接続されたホスト。:{ipAddress.ToString()} \n\n\n");
+            }
+            else
+            {
+                Console.WriteLine($"ホストへの接続に失敗しました。\nエラー内容 ={chatSystem.resultMessage}");
+            }
+        }
+
+        static void InChat()
+        {
+            ChatSystem.Buffer buffer = new ChatSystem.Buffer(maxLength);
+            bool turn = (connectMode == ChatSystem.ConnectMode.client);
+            
+            string inputSt = "";
+            string received = "";
+
+            string last_Input = "";
+
+            Console.WriteLine("しりとり開始 [先攻はホストから]");
+
+            while (true)
+            {
+                if (turn)
+                {
+                    // 受信
+                    buffer = new ChatSystem.Buffer(maxLength);
+                    ChatSystem.EResult re = chatSystem.Receive(buffer);
+
+                    if (re == ChatSystem.EResult.success)
+                    {
+                        received = Encoding.UTF8.GetString(buffer.content).Replace(EOF, "");
+
+                        if (received[0] != '\0')
+                        {
+                            // 正常にメッセージを受信
+                            Console.WriteLine($"{received}");
+                        }
+                        else
+                        {
+                            if (received.Contains("\n"))
+                            {
+                                Console.WriteLine("最後に『ん』が付いた言葉を送られた為、あなたの勝ちです。");
+                                break;
+                            }
+
+                            // 正常に終了を受信
+                            Console.WriteLine("相手から終了を受信");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        //　受信エラー
+                        Console.WriteLine($"受信エラー：{chatSystem.resultMessage} ");
+                        break;
+                    }
+
+                    bool isAns = false;
+                    last_Input = "";
+
+                    for(int ans = 0; ans < received.Length; ans++)
+                    {
+                        if (isAns)
+                        {
+                            if (received[ans] == '\0')
+                            {
+                                break;
+                            }
+
+                            last_Input += received[ans];
+                        }
+                        else if (received[ans] == ':')
+                        {
+                            isAns = true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    // 送信
+                    inputSt = Hand_Select(last_Input);
+
+                    if (inputSt.Length > maxLength)
+                    {
+                        inputSt = inputSt.Substring(0, maxLength - EOF.Length);
+                    }
+
+                    string input = user_name + inputSt + EOF;
+                    Console.WriteLine(user_name + inputSt);
+
+                    if (inputSt == "")
+                    {
+                        input = "\0";
+                    }
+                    else if (inputSt[inputSt.Length -1] == 'ん')
+                    {
+                        input = "\0" + user_name + inputSt + "\n" + EOF;
+                        Console.WriteLine("最後に『ん』が付いた言葉を送った為、あなたの負けです。");
+                    }
+
+                    buffer.content = Encoding.UTF8.GetBytes(input);
+                    buffer.length = buffer.content.Length;
+                    ChatSystem.EResult re = chatSystem.Send(buffer);
+
+                    if (re != ChatSystem.EResult.success)
+                    {
+                        Console.WriteLine($"送信エラー：{re.ToString()} エラーコード : {chatSystem.resultMessage}");
+                        break;
+                    }
+
+                    if (input[0] == '\0')
+                    {
+                        break;
+                    }
+
+                }
+
+                turn = !turn;
+            }
+
+            chatSystem.ShutDownColse();
+        }
+
+        static string Hand_Select(string last)
+        {
+            string select = "";
+
+            while (true)
+            {
+                Console.Write("日本語を入力してください。\n[空白は降参]:");
+
+                select = Console.ReadLine();
+
+                if (select == "")
+                {
+                    return "";
+                }
+
+                if (last == "" || select[0] == last[last.Length - 1])
+                {
+                    return select;
+                }
+                else
+                {
+                    Console.WriteLine("入力された言葉は前回の最後の文字から始まっていません。\nもう一度入力してください。\n");
+                }
+
+            }
+        }
+
+    }
+}
